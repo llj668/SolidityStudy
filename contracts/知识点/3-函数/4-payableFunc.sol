f8c7c1363d1aea6efc26b2bae7ee206283ef7126
Francis668 <942631528@qq.com>
2025-10-14 17:32:15 +0800

    数据类型和函数

---
+++ contracts/知识点/3-函数/4-payableFunc.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract payableFunc{
+    uint public number = 5;
+
+    function minus() internal{
+        number = number - 1;
+    }
+
+    function minusCall() external {
+        minus();
+    }
+
+
+
+    // payable: 递钱，能给合约支付eth的函数
+    function minusPayable() external payable returns(uint256 balance) {
+        minus();    
+        balance = address(this).balance;
+    }
+}
